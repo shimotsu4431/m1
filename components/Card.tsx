51a459ed0040f0d1d4b9a7de4360f1eb785da08a
shimotsu4431 <shimotsu13@gmail.com>
2021-01-03 15:02:39 +0900

    add input & label

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Card: React.FC = () => {
   return (
-    <div className="mb-4 mx-2 w-36 h-36 border-2 border-gray-400 bg-gray-200 cursor-pointer hover:bg-gray-400"></div>
+    <div className="mb-4 mx-2 w-36 h-36 border-2 border-gray-400 bg-gray-200 cursor-pointer hover:bg-gray-400">
+      <input type="checkbox" value="1" id="111" className="hidden" />
+      <label htmlFor="111"></label>
+    </div>
   )
 }
 
