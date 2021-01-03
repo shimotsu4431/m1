489ec4f0d1ad76e1c0e0a979728e182001fe8244
shimotsu4431 <shimotsu13@gmail.com>
2021-01-03 22:09:23 +0900

    fix

--- components/CardList.tsx
+++ components/CardList.tsx
@@ -85,11 +85,14 @@ const CardList: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-wrap justify-center content-between w-full max-w-screen-md mx-auto">
-      {members.length > 0 && members.map((item) => {
-        return ( <Card id={item.id} name={item.name} key={item.id} checked={val.includes(item.name)} handleChange={handleChange}></Card>)
-      })}
-    </div>
+    <>
+      <p className="text-center mb-6">選択中: {val.join(", ")}</p>
+      <div className="flex flex-wrap justify-center content-between w-full max-w-screen-md mx-auto">
+        {members.length > 0 && members.map((item) => {
+          return ( <Card id={item.id} name={item.name} key={item.id} checked={val.includes(item.name)} handleChange={handleChange}></Card>)
+        })}
+      </div>
+    </>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,24 +6,20 @@ const Home: React.FC = () => {
   return (
     <div>
       <Head>
-        <title>M-1 SELECT</title>
+        <title>SELECT UI</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="container p-4 mx-auto">
-        <section className="mb-10">
+        <section className="mb-6">
           <h1 className="text-center text-5xl font-bold mt-8 mb-4">
             M-1 SELECT
           </h1>
           <h2 className="text-center text-xl tracking-wide text-gray-400">M-1敗者復活戦風のUIを作ってみる。Tailwindも使ってみるよ。</h2>
         </section>
         <section>
-          <h2 className="text-center text-2xl font-bold mb-4">選択中</h2>
-          {/* <p>{val}</p> */}
-        </section>
-        <section>
-        <h2 className="text-center text-2xl font-bold mb-4">選択肢一覧</h2>
-        <h3 className="text-center text-base tracking-wide mb-6 text-gray-400">以下の選択肢から選んでください。</h3>
+        {/* <h2 className="text-center text-2xl font-bold mb-4">選択肢一覧</h2>
+        <h3 className="text-center text-base tracking-wide mb-6 text-gray-400">以下の選択肢から選んでください。</h3> */}
         <CardList></CardList>
       </section>
       </main>
