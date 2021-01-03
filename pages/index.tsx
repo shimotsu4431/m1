a4880908e197fdc510cdf3bf28b21ed04b1bd762
shimotsu4431 <shimotsu13@gmail.com>
2021-01-03 12:35:02 +0900

    fix title style

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,11 @@ const Home: React.FC = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="p-4">
-        <h1 className="text-center text-5xl font-bold mt-8 mb-8">
+      <main className="container p-4 mx-auto">
+        <h1 className="text-center text-5xl font-bold mt-8 mb-6">
           M-1 SELECT
         </h1>
+        <h2 className="text-center text-lg tracking-wide mb-6 text-gray-400">M-1敗者復活戦風のやつを作ってみる。Tailwindも使ってみるよ。</h2>
       </main>
     </div>
   )
