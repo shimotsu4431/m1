15d27f865efcc836932c885723f0fb00d8450299
shimotsu4431 <shimotsu13@gmail.com>
2021-01-03 12:19:44 +0900

    add tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,17 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 const Home: React.FC = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>M-1 SELECT</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main className="p-4">
+        <h1 className="text-center text-5xl font-bold mt-8 mb-8">
           M-1 SELECT
         </h1>
-        <div>
-          <h2 className={styles.subTitle}>選択中</h2>
-        </div>
       </main>
     </div>
   )
