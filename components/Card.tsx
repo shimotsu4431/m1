447cd0b8b8366a2f8b7ae7b10f8b25fd095c6ae3
shimotsu4431 <shimotsu13@gmail.com>
2021-01-03 14:51:58 +0900

    fix hover

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card: React.FC = () => {
   return (
-    <div className="mb-4 mx-2 w-36 h-36 border-2 border-gray-400 bg-gray-200 cursor-pointer"></div>
+    <div className="mb-4 mx-2 w-36 h-36 border-2 border-gray-400 bg-gray-200 cursor-pointer hover:bg-gray-400"></div>
   )
 }
 
