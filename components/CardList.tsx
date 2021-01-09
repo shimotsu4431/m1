1d4ea9cc4cf3132087cb642f9192b5adee1a6d42
shimotsu4431 <shimotsu13@gmail.com>
2021-01-09 09:42:49 +0900

    shuffle members

--- components/CardList.tsx
+++ components/CardList.tsx
@@ -1,4 +1,5 @@
-import React, { ChangeEvent, useState } from 'react'
+import { shuffle } from 'lodash'
+import React, { ChangeEvent, useMemo, useState } from 'react'
 import members from '../config/members'
 import Card from './Card'
 
@@ -13,11 +14,16 @@ type Props = {
 }
 
 const CardList: React.FC<Props> = ({val, handleChange}) => {
+  const shuffledMembers = useMemo(() => {
+    return shuffle(members)
+  }
+  , [members])
+
   return (
     <>
       <p className="text-center mb-6">選択中: {val.join(", ")}</p>
       <div className="flex flex-wrap justify-center content-between w-full max-w-screen-md mx-auto">
-        {members.length > 0 && members.map((item) => {
+        {shuffledMembers.length > 0 && shuffledMembers.map((item) => {
           return (<Card id={item.id} name={item.name} key={item.id} checked={val.includes(item.name)} handleChange={handleChange} />)
         })}
       </div>
