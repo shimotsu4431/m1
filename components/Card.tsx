13cd882a2184f60947bf2df5c41f8054c5c54f34
shimotsu4431 <shimotsu13@gmail.com>
2021-01-09 12:34:25 +0900

    add isSelected

--- components/Card.tsx
+++ components/Card.tsx
@@ -7,9 +7,10 @@ type Props = {
   src: string,
   checked: boolean,
   handleChange: (e: ChangeEvent<HTMLInputElement>) => void,
+  isSelected?: boolean
 }
 
-const Card: React.FC<Props> = ({ id, name, src, checked, handleChange }) => {
+const Card: React.FC<Props> = ({ id, name, src, checked, handleChange, isSelected }) => {
   return (
     <>
       <input className="hidden" type="checkbox" value={name} id={JSON.stringify(id)} checked={checked} onChange={handleChange} />
@@ -26,6 +27,7 @@ const Card: React.FC<Props> = ({ id, name, src, checked, handleChange }) => {
         "bg-gray-200": true,
         "cursor-pointer": true,
         "border-red-600": checked,
+        "pointer-events-none": isSelected
       })} htmlFor={JSON.stringify(id)}>
         <img className="w-36 h-36" src={src} alt=""/>
       </label>
--- components/SelectedCardList.tsx
+++ components/SelectedCardList.tsx
@@ -18,9 +18,9 @@ const SelectedCardList: React.FC<Props> = ({selectedMember, handleChange}) => {
   return (
     <>
       <div className="flex flex-wrap justify-center content-between w-full max-w-screen-md mx-auto h-36">
-        {selectedMember.length && selectedMember[0] ? <Card id={selectedMember[0].id} name={selectedMember[0].name} src={selectedMember[0].src} key={selectedMember[0].id} checked={selectedMember.includes(selectedMember[0])} handleChange={handleChange} /> :<EmptyCard />}
-        {selectedMember.length && selectedMember[1] ? <Card id={selectedMember[1].id} name={selectedMember[1].name} src={selectedMember[1].src} key={selectedMember[1].id} checked={selectedMember.includes(selectedMember[1])} handleChange={handleChange} /> :<EmptyCard />}
-        {selectedMember.length && selectedMember[2] ? <Card id={selectedMember[2].id} name={selectedMember[2].name} src={selectedMember[2].src} key={selectedMember[2].id} checked={selectedMember.includes(selectedMember[2])} handleChange={handleChange} /> :<EmptyCard />}
+        {selectedMember.length && selectedMember[0] ? <Card id={selectedMember[0].id} name={selectedMember[0].name} src={selectedMember[0].src} key={selectedMember[0].id} checked={selectedMember.includes(selectedMember[0])} handleChange={handleChange} isSelected/> :<EmptyCard />}
+        {selectedMember.length && selectedMember[1] ? <Card id={selectedMember[1].id} name={selectedMember[1].name} src={selectedMember[1].src} key={selectedMember[1].id} checked={selectedMember.includes(selectedMember[1])} handleChange={handleChange} isSelected /> :<EmptyCard />}
+        {selectedMember.length && selectedMember[2] ? <Card id={selectedMember[2].id} name={selectedMember[2].name} src={selectedMember[2].src} key={selectedMember[2].id} checked={selectedMember.includes(selectedMember[2])} handleChange={handleChange} isSelected /> :<EmptyCard />}
       </div>
     </>
   )
