1256084c51ee8487aa1e8da57f8d72235016a36b
shimotsu4431 <shimotsu13@gmail.com>
2021-01-09 12:31:19 +0900

    revert

--- components/SelectedCardList.tsx
+++ components/SelectedCardList.tsx
@@ -1,6 +1,5 @@
 import React, { ChangeEvent, useMemo, useState } from 'react'
 import members from '../config/members'
-import { CARD_NUM } from '../hooks/useSelect'
 import Card from './Card'
 import EmptyCard from './EmptyCard'
 
@@ -15,23 +14,13 @@ type Props = {
   handleChange: (e: ChangeEvent<HTMLInputElement>) => void
 }
 
-const noop = () => {}
-
 const SelectedCardList: React.FC<Props> = ({selectedMember, handleChange}) => {
   return (
     <>
       <div className="flex flex-wrap justify-center content-between w-full max-w-screen-md mx-auto h-36">
-        {(() => {
-          const items = []
-          for (let i = 0; i < CARD_NUM; i ++) {
-            selectedMember && selectedMember[i] ?
-            items.push(<Card id={selectedMember[i].id} name={selectedMember[i].name} src={selectedMember[i].src} key={selectedMember[i].id} checked={selectedMember.includes(selectedMember[i])} handleChange={noop} />)
-            :
-            items.push(<EmptyCard key={i} />)
-          }
-
-          return items
-        })()}
+        {selectedMember.length && selectedMember[0] ? <Card id={selectedMember[0].id} name={selectedMember[0].name} src={selectedMember[0].src} key={selectedMember[0].id} checked={selectedMember.includes(selectedMember[0])} handleChange={handleChange} /> :<EmptyCard />}
+        {selectedMember.length && selectedMember[1] ? <Card id={selectedMember[1].id} name={selectedMember[1].name} src={selectedMember[1].src} key={selectedMember[1].id} checked={selectedMember.includes(selectedMember[1])} handleChange={handleChange} /> :<EmptyCard />}
+        {selectedMember.length && selectedMember[2] ? <Card id={selectedMember[2].id} name={selectedMember[2].name} src={selectedMember[2].src} key={selectedMember[2].id} checked={selectedMember.includes(selectedMember[2])} handleChange={handleChange} /> :<EmptyCard />}
       </div>
     </>
   )
