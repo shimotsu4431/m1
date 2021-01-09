09e9487afff1f7c010d469daa4e4f6b8c029b9d4
shimotsu4431 <shimotsu13@gmail.com>
2021-01-09 09:36:35 +0900

    members を外部化

---
+++ config/members.ts
@@ -0,0 +1,70 @@
+import { member } from "../components/CardList"
+
+const members: member[] = [
+  {
+    id: 1,
+    name: "A",
+  },
+  {
+    id: 2,
+    name: "B",
+  },
+  {
+    id: 3,
+    name: "C",
+  },
+  {
+    id: 4,
+    name: "D",
+  },
+  {
+    id: 5,
+    name: "E",
+  },
+  {
+    id: 6,
+    name: "F",
+  },
+  {
+    id: 7,
+    name: "G",
+  },
+  {
+    id: 8,
+    name: "H",
+  },
+  {
+    id: 9,
+    name: "I",
+  },
+  {
+    id: 10,
+    name: "J",
+  },
+  {
+    id: 11,
+    name: "K",
+  },
+  {
+    id: 12,
+    name: "L",
+  },
+  {
+    id: 13,
+    name: "M",
+  },
+  {
+    id: 14,
+    name: "N",
+  },
+  {
+    id: 15,
+    name: "O",
+  },
+  {
+    id: 16,
+    name: "P",
+  }
+]
+
+export default members
