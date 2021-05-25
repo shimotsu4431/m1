1854e33f71ffb8d04651ab73baaa9c7f78c85050
shimotsu4431 <shimotsu13@gmail.com>
2021-05-25 12:35:57 +0900

    setup test

---
+++ hooks/__tests__/useSelect.ts
@@ -0,0 +1,11 @@
+import { renderHook } from "@testing-library/react-hooks"
+
+describe('true is truthy and false is falsy', () => {
+  test('true is truthy', () => {
+    expect(true).toBe(true);
+  });
+
+  test('false is falsy', () => {
+    expect(false).toBe(false);
+  });
+});
